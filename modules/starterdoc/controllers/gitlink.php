b25af6b58bca81568b5bd84bd47918d4fabc1607
DAEMonRaco <daemonraco@gmail.com>
2015-06-01 20:49:11 -0300

    Cache Expiration Time (#16)
    * Documenting cache duration usage.
    * Adding some creadits on 'starterdoc' module.

---
+++ modules/starterdoc/controllers/gitlink.php
@@ -0,0 +1,13 @@
+<?php
+
+class GitlinkController extends TooBasic\Controller {
+	//
+	// Protected properties
+	protected $_cached = true;
+	protected $_layout = false;
+	//
+	// Protected methods.
+	protected function basicRun() {
+		return true;
+	}
+}
---
+++ modules/starterdoc/controllers/mdfooter.php
@@ -0,0 +1,109 @@
+<?php
+
+class MdfooterController extends TooBasic\Controller {
+	//
+	// Protected properties
+	protected $_cached = true;
+	protected $_layout = false;
+	//
+	// Protected methods.
+	protected function basicRun() {
+		global $SkinName;
+
+		$skins = array();
+#		$skins[] = array(
+#			'value' => 'default',
+#			'title' => 'Default',
+#			'current' => false
+#		);
+		$skins[] = array(
+			'value' => 'cerulean',
+			'title' => 'Cerulean',
+			'current' => false
+		);
+#		$skins[] = array(
+#			'value' => 'cosmo',
+#			'title' => 'Cosmo',
+#			'current' => false
+#		);
+		$skins[] = array(
+			'value' => 'cyborg',
+			'title' => 'Cyborg',
+			'current' => false
+		);
+#		$skins[] = array(
+#			'value' => 'darkly',
+#			'title' => 'Darkly',
+#			'current' => false
+#		);
+#		$skins[] = array(
+#			'value' => 'flatly',
+#			'title' => 'Flatly',
+#			'current' => false
+#		);
+		$skins[] = array(
+			'value' => 'journal',
+			'title' => 'Journal',
+			'current' => false
+		);
+#		$skins[] = array(
+#			'value' => 'lumen',
+#			'title' => 'Lumen',
+#			'current' => false
+#		);
+#		$skins[] = array(
+#			'value' => 'paper',
+#			'title' => 'Paper',
+#			'current' => false
+#		);
+		$skins[] = array(
+			'value' => 'readable',
+			'title' => 'Readable',
+			'current' => false
+		);
+#		$skins[] = array(
+#			'value' => 'sandstone',
+#			'title' => 'Sandstone',
+#			'current' => false
+#		);
+		$skins[] = array(
+			'value' => 'simplex',
+			'title' => 'Simplex',
+			'current' => false
+		);
+		$skins[] = array(
+			'value' => 'slate',
+			'title' => 'Slate',
+			'current' => false
+		);
+		$skins[] = array(
+			'value' => 'spacelab',
+			'title' => 'Spacelab',
+			'current' => false
+		);
+		$skins[] = array(
+			'value' => 'superhero',
+			'title' => 'Superhero',
+			'current' => false
+		);
+		$skins[] = array(
+			'value' => 'united',
+			'title' => 'United',
+			'current' => false
+		);
+#		$skins[] = array(
+#			'value' => 'yeti',
+#			'title' => 'Yeti',
+#			'current' => false
+#		);
+		foreach($skins as &$skin) {
+			if($skin['value'] == $SkinName) {
+				$skin['current'] = true;
+				break;
+			}
+		}
+		$this->assign('skins', $skins);
+
+		return true;
+	}
+}
