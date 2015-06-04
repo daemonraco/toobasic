9cb9a31448e8583236aaafe6828fe6991d7106ee
DAEMonRaco <daemonraco@gmail.com>
2015-06-03 23:17:57 -0300

    StarterDoc
    * Better tricks.

--- modules/starterdoc/configs/config.php
+++ modules/starterdoc/configs/config.php
@@ -2,3 +2,11 @@
 
 $Defaults[GC_REQUEST_ACTION] = 'starterdoc';
 $Defaults[GC_REQUEST_SKIN] = 'united';
+/**
+ * This function restores default settings given by this module.
+ */
+function starterdoc_disable() {
+	global $Defaults;
+	$Defaults[GC_REQUEST_ACTION] = 'home';
+	$Defaults[GC_REQUEST_SKIN] = false;
+}
