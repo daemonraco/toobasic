d4a2b8e84bba384ea36e0c312aa222defe7d09c7
DAEMonRaco <daemonraco@gmail.com>
2015-08-24 14:37:58 -0300

    Code Documentation: Managers (#53)
    * Adding code documentation for:
    ** 'ServicesManager.php'
    ** 'UrlManager.php'

--- includes/managers/UrlManager.php
+++ includes/managers/UrlManager.php
@@ -10,6 +10,8 @@ namespace TooBasic\Managers;
 /**
  * @class UrlManager
  * @abstract
+ * This abstract class is just a hierarchy groupping class for sub classes that
+ * manage URL requests.
  */
 abstract class UrlManager extends Manager {
 	
