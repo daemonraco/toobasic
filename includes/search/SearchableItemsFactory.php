f315366886298a86891b78d0291c2b5c8e78626e
DAEMonRaco <daemonraco@gmail.com>
2015-12-01 12:19:00 -0300

    Search Engine (#97)
    * Adding code documentation.

--- includes/search/SearchableItemsFactory.php
+++ includes/search/SearchableItemsFactory.php
@@ -22,9 +22,20 @@ abstract class SearchableItemsFactory extends \TooBasic\Representations\ItemsFac
 	protected $_CP_NameColumn = 'name';
 	//
 	// Public methods.
+	/**
+	 * Provides access to a specific searchable items.
+	 *
+	 * @param int $id Item's id to look for.
+	 * @return \TooBasic\Search\SearchableItem Items to be analyzed.
+	 */
 	public function searchableItem($id) {
 		return $this->item($id);
 	}
+	/**
+	 * Provides access to a full list of searchable items.
+	 *
+	 * @return \TooBasic\Search\SearchableItem[] Items to be analyzed.
+	 */
 	public function searchableItems() {
 		return $this->items();
 	}
