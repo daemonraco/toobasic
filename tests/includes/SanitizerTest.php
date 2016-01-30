eb8755f5b4b3d30183edc03133defe58fa8d74bc
DAEMonRaco <daemonraco@gmail.com>
2016-01-30 20:57:57 -0300

    Travis CI
    * Adding tests for 'Sanitizer'.
    * Ignoring tests from CodeClimate.

--- tests/includes/SanitizerTest.php
+++ tests/includes/SanitizerTest.php
@@ -1,10 +1,29 @@
 <?php
 
-use TooBasic\Paths;
-
 class SanitizerTest extends PHPUnit_Framework_TestCase {
-	public function testWellFormUriGetUntouched() {
+	public function testTooManySlashes() {
+		$expected = '/some/path/script.php?parameters=value&something=else';
+		$uri = '///some/////path/script.php?parameters=value&something=else';
+		$this->assertEquals($expected, TooBasic\Sanitizer::UriPath($uri));
+	}
+	public function testWellFormmedUri() {
 		$uri = '/some/path/script.php?parameters=value&something=else';
+		$this->assertEquals($uri.'l', TooBasic\Sanitizer::UriPath($uri));
+	}
+	public function testWellFormmedSimpleUri() {
+		$uri = '/some/path/';
+		$this->assertEquals($uri, TooBasic\Sanitizer::UriPath($uri));
+	}
+	public function testWellFormmedSimpleUriNoLastSlash() {
+		$uri = '/some/path';
+		$this->assertEquals($uri, TooBasic\Sanitizer::UriPath($uri));
+	}
+	public function testWellFormmedUriNoScript() {
+		$uri = '/some/path/?parameters=value&something=else';
+		$this->assertEquals($uri, TooBasic\Sanitizer::UriPath($uri));
+	}
+	public function testWellFormmedUriNoScriptOrLastSlash() {
+		$uri = '/some/path?parameters=value&something=else';
 		$this->assertEquals($uri, TooBasic\Sanitizer::UriPath($uri));
 	}
 }
