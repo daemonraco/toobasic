90a8b7d085d8c16fd61ecafe3114a52f2d07530c
DAEMonRaco <daemonraco@gmail.com>
2016-01-31 00:27:37 -0300

    Travis CI & PHPUnit
    * More test-cases for class 'Names'.

--- tests/includes/NamesTest.php
+++ tests/includes/NamesTest.php
@@ -45,11 +45,100 @@ class NamesTest extends PHPUnit_Framework_TestCase {
 	}
 	// @}
 	//
+	// Filename names @{
+	public function testConfigFilename() {
+		$this->myBasicFilenameTester('ConfigFilename');
+	}
+	public function testControllerFilename() {
+		$this->myBasicFilenameTester('ControllerFilename');
+	}
+	public function testEmailControllerFilename() {
+		$this->myBasicFilenameTester('EmailControllerFilename');
+	}
+	public function testItemRepresentationFilename() {
+		$this->assertEquals('MyFileRepresentation', TooBasic\Names::ItemRepresentationFilename('my_file'));
+		$this->assertEquals('MyFileRepresentation', TooBasic\Names::ItemRepresentationFilename('MyFile'));
+		$this->assertEquals('MyFileRepresentation', TooBasic\Names::ItemRepresentationFilename('my-file'));
+		$this->assertEquals('MyFileRepresentation', TooBasic\Names::ItemRepresentationFilename('my file'));
+
+		$this->assertEquals('MyFileRepresentation', TooBasic\Names::ItemRepresentationFilename('my_file_representation'));
+		$this->assertEquals('MyFileRepresentation', TooBasic\Names::ItemRepresentationFilename('MyFileRepresentation'));
+		$this->assertEquals('MyFileRepresentation', TooBasic\Names::ItemRepresentationFilename('my-file-representation'));
+		$this->assertEquals('MyFileRepresentation', TooBasic\Names::ItemRepresentationFilename('my file representation'));
+	}
+	public function testItemsFactoryFilename() {
+		$this->assertEquals('MyFileFactory', TooBasic\Names::ItemsFactoryFilename('my_file'));
+		$this->assertEquals('MyFileFactory', TooBasic\Names::ItemsFactoryFilename('MyFile'));
+		$this->assertEquals('MyFileFactory', TooBasic\Names::ItemsFactoryFilename('my-file'));
+		$this->assertEquals('MyFileFactory', TooBasic\Names::ItemsFactoryFilename('my file'));
+
+		$this->assertEquals('MyFileFactory', TooBasic\Names::ItemsFactoryFilename('my_file_factory'));
+		$this->assertEquals('MyFileFactory', TooBasic\Names::ItemsFactoryFilename('MyFileFactory'));
+		$this->assertEquals('MyFileFactory', TooBasic\Names::ItemsFactoryFilename('my-file-factory'));
+		$this->assertEquals('MyFileFactory', TooBasic\Names::ItemsFactoryFilename('my file factory'));
+	}
+	public function testModelFilename() {
+		$this->myUpperFilenameTester('ModelFilename');
+	}
+	public function testServiceFilename() {
+		$this->myBasicFilenameTester('ServiceFilename');
+	}
+	public function testShellCronFilename() {
+		$this->myBasicFilenameTester('ShellCronFilename');
+	}
+	public function testShellSysFilename() {
+		$this->myBasicFilenameTester('ShellSysFilename');
+	}
+	public function testShellToolFilename() {
+		$this->myBasicFilenameTester('ShellToolFilename');
+	}
+	// @}
+	//
+	// Generic methods @{
+	public function testClassName() {
+		$this->myClassTester('MyClass', 'ClassName');
+	}
+	public function testClassNameWithSuffix() {
+		$expected = 'MyClassSuffix';
+		$this->assertEquals($expected, TooBasic\Names::ClassNameWithSuffix('MyClass', 'Suffix'));
+		$this->assertEquals($expected, TooBasic\Names::ClassNameWithSuffix('my_class', 'Suffix'));
+		$this->assertEquals($expected, TooBasic\Names::ClassNameWithSuffix('my class', 'Suffix'));
+		$this->assertEquals($expected, TooBasic\Names::ClassNameWithSuffix('my-class', 'Suffix'));
+		$this->assertEquals($expected, TooBasic\Names::ClassNameWithSuffix('My Class', 'Suffix'));
+
+		$expected = 'MySuffix';
+		$this->assertEquals($expected, TooBasic\Names::ClassNameWithSuffix('MySuffix', 'Suffix'));
+		$this->assertEquals($expected, TooBasic\Names::ClassNameWithSuffix('my_suffix', 'Suffix'));
+		$this->assertEquals($expected, TooBasic\Names::ClassNameWithSuffix('my suffix', 'Suffix'));
+		$this->assertEquals($expected, TooBasic\Names::ClassNameWithSuffix('my-suffix', 'Suffix'));
+		$this->assertEquals($expected, TooBasic\Names::ClassNameWithSuffix('My Suffix', 'Suffix'));
+	}
+	public function testFilename() {
+		$this->myUpperFilenameTester('Filename');
+	}
+	public function testSnakeFilename() {
+		$this->myBasicFilenameTester('SnakeFilename');
+	}
+	// @}
+	//
 	// Internal methods.
 	protected function myClassTester($expected, $func,$suffixName = 'class') {
+		$this->assertEquals($expected, TooBasic\Names::$func($expected));
 		$this->assertEquals($expected, TooBasic\Names::$func("my_{$suffixName}"));
 		$this->assertEquals($expected, TooBasic\Names::$func("my {$suffixName}"));
 		$this->assertEquals($expected, TooBasic\Names::$func("my-{$suffixName}"));
 		$this->assertEquals($expected, TooBasic\Names::$func(ucwords("my {$suffixName}")));
+	}
+	protected function myBasicFilenameTester($func) {
+		$this->assertEquals('my_file', TooBasic\Names::{$func}('my_file'));
+		$this->assertEquals('my_file', TooBasic\Names::{$func}('MyFile'));
+		$this->assertEquals('my-file', TooBasic\Names::{$func}('my-file'));
+		$this->assertEquals('my-file', TooBasic\Names::{$func}('my file'));
+	}
+	protected function myUpperFilenameTester($func) {
+		$this->assertEquals('MyFile', TooBasic\Names::{$func}('my_file'));
+		$this->assertEquals('MyFile', TooBasic\Names::{$func}('MyFile'));
+		$this->assertEquals('MyFile', TooBasic\Names::{$func}('my-file'));
+		$this->assertEquals('MyFile', TooBasic\Names::{$func}('my file'));
 	}
 }
