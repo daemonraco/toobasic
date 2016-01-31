1ec108c90fc230684011d6325c3e959fe003aa54
DAEMonRaco <daemonraco@gmail.com>
2016-01-30 23:32:23 -0300

    Travis CI
    * Adding an author's note about PHPUnit and it small amount of cases.
    * More test-cases.
    * Spell checks.

--- tests/includes/SanitizerTest.php
+++ tests/includes/SanitizerTest.php
@@ -1,29 +1,57 @@
 <?php
 
 class SanitizerTest extends PHPUnit_Framework_TestCase {
-	public function testTooManySlashes() {
+	//
+	// Testing Sanitizer::DirPath() @{
+	public function testPathWithTooManySlashes() {
+		$expected = '/some/path/script.php';
+		$dir = '///some/////path/script.php';
+		$this->assertEquals($expected, TooBasic\Sanitizer::DirPath($dir));
+	}
+	public function testAbosolutePathWithTooManySlashes() {
+		$expected = 'some/path/script.php';
+		$dir = 'some/////path/script.php';
+		$this->assertEquals($expected, TooBasic\Sanitizer::DirPath($dir));
+	}
+	public function testWellFormedPath() {
+		$dir = 'some/path/script.php';
+		$this->assertEquals($dir, TooBasic\Sanitizer::DirPath($dir));
+	}
+	public function testWellFormedAbsolutePath() {
+		$dir = '/some/path/script.php';
+		$this->assertEquals($dir, TooBasic\Sanitizer::DirPath($dir));
+	}
+	public function testWellFormedPathWithEndingSlash() {
+		$dir = '/some/path/';
+		$this->assertEquals($dir, TooBasic\Sanitizer::DirPath($dir));
+	}
+	// @}
+	//
+	// Testing Sanitizer::UriPath() @{
+	public function testUriWithTooManySlashes() {
 		$expected = '/some/path/script.php?parameters=value&something=else';
 		$uri = '///some/////path/script.php?parameters=value&something=else';
 		$this->assertEquals($expected, TooBasic\Sanitizer::UriPath($uri));
 	}
-	public function testWellFormmedUri() {
+	public function testWellFormedUri() {
 		$uri = '/some/path/script.php?parameters=value&something=else';
 		$this->assertEquals($uri, TooBasic\Sanitizer::UriPath($uri));
 	}
-	public function testWellFormmedSimpleUri() {
+	public function testWellFormedSimpleUri() {
 		$uri = '/some/path/';
 		$this->assertEquals($uri, TooBasic\Sanitizer::UriPath($uri));
 	}
-	public function testWellFormmedSimpleUriNoLastSlash() {
+	public function testWellFormedSimpleUriNoLastSlash() {
 		$uri = '/some/path';
 		$this->assertEquals($uri, TooBasic\Sanitizer::UriPath($uri));
 	}
-	public function testWellFormmedUriNoScript() {
+	public function testWellFormedUriNoScript() {
 		$uri = '/some/path/?parameters=value&something=else';
 		$this->assertEquals($uri, TooBasic\Sanitizer::UriPath($uri));
 	}
-	public function testWellFormmedUriNoScriptOrLastSlash() {
+	public function testWellFormedUriNoScriptOrLastSlash() {
 		$uri = '/some/path?parameters=value&something=else';
 		$this->assertEquals($uri, TooBasic\Sanitizer::UriPath($uri));
 	}
+	// @}
 }
