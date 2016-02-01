f6835e028bf941ce700e02364ec3e5312447b720
DAEMonRaco <daemonraco@gmail.com>
2016-02-01 10:47:51 -0300

    Selenium on Travis CI Tests
    * More configuration tests (I don't like to install tons of extra stuff).

--- tests/SeleniumTest.php
+++ tests/SeleniumTest.php
@@ -2,10 +2,10 @@
 class WebTest extends PHPUnit_Extensions_Selenium2TestCase {
 	protected function setUp() {
 		$this->setBrowser('firefox');
-		$this->setBrowserUrl('http://toobasic.daemonraco.com/');
+		$this->setBrowserUrl('http://localhost/');
 	}
 	public function testTitle() {
 		$this->url('index.php');
-		$this->assertEquals('TooBasic-1.0.4-serpent', $this->title());
+		$this->assertEquals('TooBasic', $this->title());
 	}
 }
