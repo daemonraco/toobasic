9248dea9390b470bd91f473d86756abe5d3aef6a
DAEMonRaco <daemonraco@gmail.com>
2016-02-01 10:57:46 -0300

    Selenium on Travis CI Tests
    * Test and configuration twiks.

--- tests/SeleniumTest.php
+++ tests/SeleniumTest.php
@@ -1,8 +1,10 @@
 <?php
 class WebTest extends PHPUnit_Extensions_Selenium2TestCase {
 	protected function setUp() {
+		$this->setHost('localhost');
+		$this->setPort('80');
 		$this->setBrowser('firefox');
-		$this->setBrowserUrl('http://localhost/');
+		$this->setBrowserUrl('http://localhost:80/');
 	}
 	public function testTitle() {
 		$this->url('index.php');
