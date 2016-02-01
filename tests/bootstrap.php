ba8f970349f0ed7b2843ecc0e2d4569a487ad511
DAEMonRaco <daemonraco@gmail.com>
2016-02-01 15:31:07 -0300

    Selenium on Travis CI Tests
    * Test and configuration twiks & fixes.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -24,3 +24,9 @@ global $SuperLoader;
 require_once dirname(__DIR__).'/vendor/autoload.php';
 require_once __DIR__.'/TooBasic_TestCase.php';
 require_once dirname(__DIR__).'/config/config.php';
+// @fixme remove this travis test @{
+debugit(array(
+	'PHP_VERSION' => PHP_VERSION,
+	'phpversion()' > phpversion()
+));
+// @}
