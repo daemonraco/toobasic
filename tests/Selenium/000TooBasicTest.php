7c592b95a28995a52132e55c2c1fadf03494e12e
DAEMonRaco <daemonraco@gmail.com>
2016-02-01 18:26:55 -0300

    Selenium on Travis CI Tests
    * At least for now, TooBasic won't be aimed for HHVM.
    * Creating a generic TooBasic-Selenium test-case class.
    * Making Selenium cases more flexible to run on TravisCI and Development environments.
    * Ignoring some composer files and folders.

--- tests/Selenium/000TooBasicTest.php
+++ tests/Selenium/000TooBasicTest.php
@@ -1,12 +1,6 @@
 <?php
 
-class TooBasicTest extends PHPUnit_Extensions_Selenium2TestCase {
-	protected function setUp() {
-		$this->setHost('localhost');
-		$this->setPort(4444);
-		$this->setBrowser('firefox');
-		$this->setBrowserUrl('http://localhost/');
-	}
+class TooBasicTest extends TooBasic_SeleniumTestCase {
 	public function testTitle() {
 		$this->url('index.php');
 		$this->assertRegExp('/TooBasic/', $this->title(), "Page title doesn't have the keyword 'TooBasic'.");
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -23,4 +23,10 @@ global $SuperLoader;
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
 require_once __DIR__.'/TooBasic_TestCase.php';
+require_once __DIR__.'/TooBasic_SeleniumTestCase.php';
 require_once dirname(__DIR__).'/config/config.php';
+//
+// Loading TravisCI variables.
+foreach(array('TRAVISCI_URL_SCHEME', 'TRAVISCI_URL_PORT', 'TRAVISCI_URI') as $var) {
+	define($var, getenv($var));
+}
