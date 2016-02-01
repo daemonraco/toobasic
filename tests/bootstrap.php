2256b66ff1890fb10bba14f35e0668a648bb3faa
DAEMonRaco <daemonraco@gmail.com>
2016-02-01 09:54:08 -0300

    Selenium on Travis CI Tests
    * Adding configuration to load Selenium test cases definitions from composer.
    * Using the composer loader.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -21,5 +21,6 @@ global $ServiceName;
 global $SkinName;
 global $SuperLoader;
 
+require_once dirname(__DIR__).'/vendor/autoload.php';
 require_once __DIR__.'/TooBasic_TestCase.php';
 require_once dirname(__DIR__).'/config/config.php';
