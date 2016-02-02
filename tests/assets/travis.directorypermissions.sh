da1199cc7c86d334a2cde454cae3bce4243835c3
DAEMonRaco <daemonraco@gmail.com>
2016-02-02 00:34:08 -0300

    Test Cases
    * Pre-generating smarty folders to avoid issues with permissions.

--- tests/assets/travis.directorypermissions.sh
+++ tests/assets/travis.directorypermissions.sh
@@ -1,6 +1,8 @@
 sudo chmod -v 0777 cache
 cd cache
-mkdir -vp smarty/{cache,compile,configs}
+mkdir -vp smarty/cache
+mkdir -vp smarty/compile
+mkdir -vp smarty/configs
 sudo find . -type d | xargs sudo chmod -v 0777
 sudo find . -type f | xargs sudo rm -v
 git checkout .
