fb28fb311cdff9bd7838a2ccbf910b1bee25defa
DAEMonRaco <daemonraco@gmail.com>
2016-02-08 00:25:01 -0300

    Test Cases
    * Trying MySQL usage in a different way for TravisCI... again

--- tests/assets/travis.mysqlconfig.sh
+++ tests/assets/travis.mysqlconfig.sh
@@ -1,4 +1,5 @@
-sudo mysql -u root -e "CREATE USER '${TRAVISCI_MYSQL_USERNAME}'@'%' IDENTIFIED BY '${TRAVISCI_MYSQL_PASSWORD}'" 2>&1
-sudo mysql -u root -e "CREATE USER '${TRAVISCI_MYSQL_USERNAME}'@'localhost' IDENTIFIED BY '${TRAVISCI_MYSQL_PASSWORD}'" 2>&1
-sudo mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${TRAVISCI_MYSQL_USERNAME}'@'%'" 2>&1
-sudo mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${TRAVISCI_MYSQL_USERNAME}'@'localhost'" 2>&1
+sudo mysql -u root -e "CREATE USER '${TRAVISCI_MYSQL_USERNAME}'@'%' IDENTIFIED BY '${TRAVISCI_MYSQL_PASSWORD}'" 2>&1;
+sudo mysql -u root -e "CREATE USER '${TRAVISCI_MYSQL_USERNAME}'@'localhost' IDENTIFIED BY '${TRAVISCI_MYSQL_PASSWORD}'" 2>&1;
+sudo mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${TRAVISCI_MYSQL_USERNAME}'@'%'" 2>&1;
+sudo mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${TRAVISCI_MYSQL_USERNAME}'@'localhost'" 2>&1;
+sudo mysql -u root -e "UPDATE mysql.user SET plugin='mysql_native_password' WHERE User = '${TRAVISCI_MYSQL_USERNAME}'; FLUSH PRIVILEGES" 2>&1;
