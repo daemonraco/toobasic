41bf04361017a84d8d4d61cd34ffa78295d08be0
DAEMonRaco <daemonraco@gmail.com>
2016-02-08 00:30:00 -0300

    Test Cases
    * Trying MySQL usage in a different way for TravisCI... again

--- tests/assets/travis.mysqlconfig.sh
+++ tests/assets/travis.mysqlconfig.sh
@@ -2,4 +2,5 @@ sudo mysql -u root -e "CREATE USER '${TRAVISCI_MYSQL_USERNAME}'@'%' IDENTIFIED B
 sudo mysql -u root -e "CREATE USER '${TRAVISCI_MYSQL_USERNAME}'@'localhost' IDENTIFIED BY '${TRAVISCI_MYSQL_PASSWORD}'" 2>&1;
 sudo mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${TRAVISCI_MYSQL_USERNAME}'@'%'" 2>&1;
 sudo mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${TRAVISCI_MYSQL_USERNAME}'@'localhost'" 2>&1;
-sudo mysql -u root -e "UPDATE mysql.user SET plugin='mysql_native_password' WHERE User = '${TRAVISCI_MYSQL_USERNAME}'; FLUSH PRIVILEGES" 2>&1;
+sudo mysql -u root -e "UPDATE mysql.user SET plugin='' WHERE User = '${TRAVISCI_MYSQL_USERNAME}'; FLUSH PRIVILEGES" 2>&1;
+sudo mysql -u root -e "select * FROM mysql.user" 2>&1;
