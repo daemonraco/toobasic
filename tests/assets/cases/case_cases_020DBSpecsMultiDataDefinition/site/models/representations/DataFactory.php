637c16284e37de19355a82068d33d28a5383a537
DAEMonRaco <daemonraco@gmail.com>
2016-02-14 13:13:10 -0300

    Test Cases
    * Adding a case to test multi-data definition on database specifications.

---
+++ tests/assets/cases/case_cases_020DBSpecsMultiDataDefinition/site/models/representations/DataFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+class DataFactory extends \TooBasic\Representations\ItemsFactory {
+	//
+	// Protected core properties.
+	protected $_CP_ColumnsPerfix = 'dat_';
+	protected $_CP_IDColumn = 'id';
+	protected $_CP_RepresentationClass = 'datum';
+	protected $_CP_Table = 'data';
+}
