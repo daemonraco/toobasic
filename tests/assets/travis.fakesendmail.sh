1b65e4c404bad6924e476b342c966db15c511724
DAEMonRaco <daemonraco@gmail.com>
2016-02-15 20:39:18 -0300

    Test Cases With Emails
    * Debug messages and some IFs.

--- tests/assets/travis.fakesendmail.sh
+++ tests/assets/travis.fakesendmail.sh
@@ -2,6 +2,7 @@
 #
 # Create a fake mail-box folder.
 mailbox="${TRAVISCI_EMAIL_TMP-/tmp/}fake-mailbox";
+echo "mailbox=${mailbox}" > /home/travis/build/daemonraco/toobasic/fakemail.txt;
 mkdir -p $mailbox;
 
 if [ ! -f $mailbox/counter ]; then
@@ -15,6 +16,7 @@ echo $count > $mailbox/counter;
 name="${mailbox}/message_${count}.eml";
 while read line; do
 	echo $line >> $name;
+	echo "${line}" >> /home/travis/build/daemonraco/toobasic/fakemail.txt;
 done;
 
 exit 0
