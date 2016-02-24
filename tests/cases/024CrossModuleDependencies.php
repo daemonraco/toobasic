2ec351b76165c67dfb6353354301b48594e8a3d1
DAEMonRaco <daemonraco@gmail.com>
2016-02-24 11:16:55 -0300

    Test Cases
    * New case to check dependencies between modules (#71).

---
+++ tests/cases/024CrossModuleDependencies.php
@@ -0,0 +1,10 @@
+<?php
+
+class CrossModuleDependenciesTest extends TooBasic_TestCase {
+	//
+	// Test cases @{
+	public function testCheckingAProperLoading() {
+		$json = $this->getJSONUrl('?action=test&format=json');
+	}
+	// @}
+}
