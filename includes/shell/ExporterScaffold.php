994350a3e1d1e059640ca696e9a9712646730aab
DAEMonRaco <daemonraco@gmail.com>
2016-08-19 11:25:17 -0300

    Scaffolds with default values in routes (#115)
    * Bug fix.

--- includes/shell/ExporterScaffold.php
+++ includes/shell/ExporterScaffold.php
@@ -26,53 +26,51 @@ class ExporterScaffold extends Scaffold {
 			//
 			// Controller's route.
 			$opt = $this->_options->option(self::OptionParam);
-			if($opt->activated()) {
+			//
+			// Expanding parameters.
+			$params = [];
+			$options = $opt->activated() ? $opt->value() : [];
+			foreach($options as $param) {
+				$pieces = explode(':', $param);
+				$params[] = [
+					GC_AFIELD_NAME => $pieces[0],
+					GC_AFIELD_DEFAULT => isset($pieces[1]) ? $pieces[1] : false
+				];
+			}
+			$paramsCount = count($params);
+			//
+			// Generating mutiple routes with decreasing sizes.
+			for($currentCount = $paramsCount; $currentCount >= 0; $currentCount--) {
 				//
-				// Expanding parameters.
-				$params = [];
-				foreach($opt->value() as $param) {
-					$pieces = explode(':', $param);
-					$params[] = [
-						GC_AFIELD_NAME => $pieces[0],
-						GC_AFIELD_DEFAULT => isset($pieces[1]) ? $pieces[1] : false
-					];
+				// If the next parameter has no default value this
+				// and next routes with shorter sizes won't be
+				// valid.
+				if(isset($params[$currentCount]) && $params[$currentCount][GC_AFIELD_DEFAULT] === false) {
+					break;
 				}
-				$paramsCount = count($params);
 				//
-				// Generating mutiple routes with decreasing
-				// sizes.
-				for($currentCount = $paramsCount; $currentCount >= 0; $currentCount--) {
-					//
-					// If the next parameter has no default
-					// value this and next routes with shorter
-					// sizes won't be valid.
-					if(isset($params[$currentCount]) && $params[$currentCount][GC_AFIELD_DEFAULT] === false) {
-						break;
-					}
-					//
-					// Building a route @{
-					$route = new \stdClass();
-					$route->route = "{$this->_genRoutePrefix}{$this->_names[GC_AFIELD_NAME]}";
-					for($i = 0; $i < $currentCount; $i++) {
-						$route->route .= "/:{$params[$i][GC_AFIELD_NAME]}:";
-					}
-					$route->{$this->_genRouteType} = $this->_names[GC_AFIELD_NAME];
-					//
-					//Building route parameters.
-					$route->params = new \stdClass();
-					$hasDefaults = false;
-					for($i = $paramsCount - 1; $i >= $currentCount; $i--) {
-						$hasDefaults = true;
-						$route->params->{$params[$i][GC_AFIELD_NAME]} = $params[$i][GC_AFIELD_DEFAULT];
-					}
-					if(!$hasDefaults) {
-						unset($route->params);
-					}
-					// @}
-					//
-					// Adding the new route.
-					$this->_routes[] = $route;
+				// Building a route @{
+				$route = new \stdClass();
+				$route->route = "{$this->_genRoutePrefix}{$this->_names[GC_AFIELD_NAME]}";
+				for($i = 0; $i < $currentCount; $i++) {
+					$route->route .= "/:{$params[$i][GC_AFIELD_NAME]}:";
 				}
+				$route->{$this->_genRouteType} = $this->_names[GC_AFIELD_NAME];
+				//
+				//Building route parameters.
+				$route->params = new \stdClass();
+				$hasDefaults = false;
+				for($i = $paramsCount - 1; $i >= $currentCount; $i--) {
+					$hasDefaults = true;
+					$route->params->{$params[$i][GC_AFIELD_NAME]} = $params[$i][GC_AFIELD_DEFAULT];
+				}
+				if(!$hasDefaults) {
+					unset($route->params);
+				}
+				// @}
+				//
+				// Adding the new route.
+				$this->_routes[] = $route;
 			}
 		}
 	}
