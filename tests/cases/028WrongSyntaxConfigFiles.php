843cc61b287974c26959cf6bf386f8bfe7e9275a
DAEMonRaco <daemonraco@gmail.com>
2016-09-23 00:00:35 -0300

    JSON Validator for SApiReports (#173)
    * Updating test cases.

--- tests/cases/028WrongSyntaxConfigFiles.php
+++ tests/cases/028WrongSyntaxConfigFiles.php
@@ -56,7 +56,7 @@ class WrongSyntaxConfigFilesTest extends TooBasic_TestCase {
 		$this->assertRegExp(ASSERTION_PATTERN_TOOBASIC_EXCEPTION, $response, "Response to '{$url}' doesn't have a TooBasic exception.");
 		$this->assertNotRegExp(ASSERTION_PATTERN_PHP_ERROR, $response, "Response to '{$url}' seems to have a PHP error.");
 
-		$this->assertRegExp("~(.*){$asset}(.*)is not a valid JSON~", $response, "Response to '{$url}' doesn't mention the broken file.");
+		$this->assertRegExp("~JSON file at '/(.*)\.json' doesn't match the specifications~", $response, "Response to '{$url}' doesn't mention the broken file.");
 	}
 	public function testCheckingBrokenFormsSpecification() {
 		$asset = '/site/forms/broken_form.json';
