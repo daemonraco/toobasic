c7f251b1c9ddd10de06a97bbd763ab2f3d131318
DAEMonRaco <daemonraco@gmail.com>
2016-11-29 12:04:30 -0300

    Deprecated Functionalities (v2.3.0) (#188)
    * Adapting test cases.

--- tests/assets/cases/cases/020DBSpecsMultiDataDefinition/site/models/representations/DataFactory.php
+++ tests/assets/cases/cases/020DBSpecsMultiDataDefinition/site/models/representations/DataFactory.php
@@ -1,10 +1,5 @@
 <?php
 
 class DataFactory extends \TooBasic\Representations\ItemsFactory {
-	//
-	// Protected core properties.
-	protected $_CP_ColumnsPerfix = 'dat_';
-	protected $_CP_IDColumn = 'id';
-	protected $_CP_RepresentationClass = 'datum';
-	protected $_CP_Table = 'data';
+	protected $_corePropsHolder = 'data';
 }
--- tests/assets/cases/cases/032Rest/site/models/representations/AuthsFactory.php
+++ tests/assets/cases/cases/032Rest/site/models/representations/AuthsFactory.php
@@ -1,10 +1,5 @@
 <?php
 
 class AuthsFactory extends \TooBasic\Representations\ItemsFactory {
-	//
-	// Protected core properties.
-	protected $_CP_ColumnsPerfix = 'auth_';
-	protected $_CP_IDColumn = 'id';
-	protected $_CP_RepresentationClass = 'auth';
-	protected $_CP_Table = 'auths';
+	protected $_corePropsHolder = 'auths';
 }
